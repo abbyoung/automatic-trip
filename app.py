1e98794831f9535b48fec55dd06108ce02cfa482
Abigail Young <Abigail@sherry.local>
2015-03-30 15:29:43 -0700

    checking for config var

--- app.py
+++ app.py
@@ -15,8 +15,11 @@ app = Flask(__name__)
 def index():
     # Get recent 10 trips from Automatic
     # Using sandbox Automatic user
-    r = requests.get('https://api.automatic.com/v1/trips?per_page=10', headers={'Authorization': 'token '+os.environ["AUTOMATIC_KEY"]})
-    recent_trips = r.json()
+    try:
+        r = requests.get('https://api.automatic.com/v1/trips?per_page=10', headers={'Authorization': 'token '+os.environ["AUTOMATIC_KEY"]})
+        recent_trips = r.json()
+    except Exception, e:
+        return 'This is where the error handling would go.'
 
     # Get dates of trips
     for i in range(len(recent_trips)):
@@ -25,26 +28,29 @@ def index():
 
     # Get friends from Venmo
     # Using sandbox api with personal Venmo ID. Add your own to see your friends!
-    friends = requests.get('https://sandbox-api.venmo.com/v1/users/974538061905920884/friends?access_token='+os.environ["VENMO_KEY"]+'&limit=1000')
-    friends = friends.json()['data']
+    try:
+        friends = requests.get('https://sandbox-api.venmo.com/v1/users/974538061905920884/friends?access_token='+os.environ["VENMO_KEY"]+'&limit=1000')
+        friends = friends.json()['data']
+    except Exception, e:
+        return 'This is where the error handling would go.'
     
-
     return render_template("index.html", recent_trips=recent_trips, friends=friends, venmo_key=os.environ['VENMO_KEY'], map_key=os.environ['MAP_KEY'])
 
 @app.route("/payment", methods=["POST"])
 def payment(): 
-    try:
-        trip_id = request.form['trip_id']
-    except Exception, e:
-        print 'NO JSON OBJO YO'
-        flash('Couldn\'nt charge your friend. Try again!')
-        #TODO: Add flash to template
-        return 403
+
+    trip_id = request.form['trip_id']
+       
     # If using the real Venmo API, the following data would be passed
-    # data = {'access_token': os.environ['VENMO_KEY'], 'user_id': request.form['user_id'], 'amount': '-0.20', 'note': 'Gas charge'}
+    if os.environ.get('CONFIG') == 'production':
+        data = {'access_token': os.environ['VENMO_KEY'], 'user_id': request.form['user_id'], 'amount': "-"+(request.form['price']), 'note': 'Gas charge'}
+        r = requests.post('https://api.venmo.com/v1/payments?access_token=', data=data)
+    
     # But we have to hard-code sandbox user details to comply with sandbox API
-    data = {'access_token': os.environ['VENMO_KEY'], 'user_id': '145434160922624933', 'amount': '-0.20', 'note': 'Gas charge'}
-    r = requests.post('https://sandbox-api.venmo.com/v1/payments?access_token=', data=data)
+    else:
+        data = {'access_token': os.environ['VENMO_KEY'], 'user_id': '145434160922624933', 'amount': '-0.20', 'note': 'Gas charge'}
+        r = requests.post('https://sandbox-api.venmo.com/v1/payments?access_token=', data=data)
+    
     payment = json.dumps(r.json())
     return payment
 
